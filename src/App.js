54c5d0e202a167da9e61b081ffb6d34d4ac0715f
Jack Woodrow <jackwoodrow99@gmail.com>
2023-10-15 20:26:13 -0400

    Basic page layout

--- src/App.js
+++ src/App.js
@@ -13,19 +13,19 @@ function App() {
     <AppContext.Provider
       value={{}}
     >
-      <header>
+      <header className='w-full h-16 px-16 bg-slate-900 flex flex-row items-center space-x-4 text-white'>
         <A to={'/'}>Home</A>
         <A to={'/auth/login'}>Login</A>
         <A to={'/auth/register'}>Register</A>
         <A to={'/auth/verify'}>Verify</A>
       </header>
 
-      <main>
+      <main className='w-full min-h-[calc(100vh-64px-64px)] p-4'>
         <Router/>
       </main>
 
-      <footer>
-        <p>Footer</p>
+      <footer className='w-full h-16 px-16 bg-slate-900 flex flex-row items-center justify-center space-x-4 text-white'>
+        <p className='text-center	'>Footer</p>
       </footer>
     </AppContext.Provider>
   );
